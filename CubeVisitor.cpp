cdf2793eca92c10f8b62700ad5c93e0c8afc3de9
Radosław Jarzynka <radoslaw.jarzynka@gmail.com>
2014-04-24 16:19:32 +0200

    po usunięciu

--- CubeVisitor.cpp
+++
@@ -1,7 +0,0 @@
-#include "CubeVisitor.h"
-
-void CubeVisitor::visit(WoodenCube& wC) {}
-void CubeVisitor::visit(MetalCube& mC) {}
-void CubeVisitor::visit(WaterCube& wC) {}
-void CubeVisitor::visit(FireCube& fC) {}
-void CubeVisitor::visit(GoldCube& gC) {}
