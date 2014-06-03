7fe07d3d0b7c3fde606af30f32ef47a955f0f108
Radosław Jarzynka <radoslaw.jarzynka@gmail.com>
2014-06-04 00:42:25 +0200

    usunieto komentarze i poprawiono destruktor kloca

--- Cube.cpp
+++ Cube.cpp
@@ -80,51 +80,7 @@ Cube::~Cube() {
     _cubeNode->removeAndDestroyAllChildren ();
     _mSceneMgr->destroySceneNode(_name);
 }
-/*
-void Cube::ground(Ogre::SceneManager& mSceneMgr) {
-	
-	Ogre::SceneManager::MovableObjectIterator it= mSceneMgr.getMovableObjectIterator("Entity");
-	Ogre::Real lowestY = 0;
-	std::vector<std::string> _temp = split(_cubeNode->getName(), ':');
-	// 0:'Cube' 1:'x' 2:'y' 3:'z'
-	int x;
-	std::istringstream iss1(_temp[1]);
-	iss1 >> x;
-	int y;
-	std::istringstream iss2(_temp[2]);
-	iss2 >> y;
-	int z;
-	std::istringstream iss3(_temp[3]);
-	iss3 >> z;
-	while (it.hasMoreElements())
-	{
-		
-		std::string _name = it.getNext()->getName();
 
-		try {
-			std::vector<std::string> _temp = split(_name, ':');
-			// 0:'Cube' 1:'x' 2:'y' 3:'z'
-			int _tempX;
-			std::istringstream iss1(_temp[1]);
-			iss1 >> _tempX;
-			int _tempY;
-			std::istringstream iss2(_temp[2]);
-			iss2 >> _tempY;
-			int _tempZ;
-			std::istringstream iss3(_temp[3]);
-			iss3 >> _tempZ;
-			if (_tempX > (x - 25) && _tempX < (x + 25) && _tempZ > (z - 25) && _tempZ < (z + 25))	
-			{
-				if (lowestY <= _tempY) lowestY = _tempY+25;
-			}
-
-			_cubeNode->setPosition(x, lowestY, z);
-		}
-		catch (...) {    }
-
-	}
-}
-*/
 Ogre::SceneNode* Cube::getCubeNode() {
 	return _cubeNode;
 }
@@ -146,10 +102,7 @@ FireCube::FireCube(Ogre::SceneManager& mSceneMgr, Ogre::Real x, Ogre::Real y, Og
 FireCube::~FireCube()
 {
 }
-/*
-void FireCube::accept(CubeVisitor& cV) {
-	cV.visit(*this);
-}*/
+
 
 GoldCube::GoldCube(Ogre::SceneManager& mSceneMgr, Ogre::Real x, Ogre::Real y, Ogre::Real z) : Cube(mSceneMgr, x, y, z)
 {
@@ -160,10 +113,6 @@ GoldCube::GoldCube(Ogre::SceneManager& mSceneMgr, Ogre::Real x, Ogre::Real y, Og
 GoldCube::~GoldCube()
 {
 }
-/*
-void GoldCube::accept(CubeVisitor& cV) {
-	cV.visit(*this);
-}*/
 
 MetalCube::MetalCube(Ogre::SceneManager& mSceneMgr, Ogre::Real x, Ogre::Real y, Ogre::Real z) : Cube(mSceneMgr, x, y, z)
 {
@@ -174,10 +123,7 @@ MetalCube::MetalCube(Ogre::SceneManager& mSceneMgr, Ogre::Real x, Ogre::Real y, 
 MetalCube::~MetalCube()
 {
 }
-/*
-void MetalCube::accept(CubeVisitor& cV) {
-	cV.visit(*this);
-}*/
+
 
 
 
@@ -191,10 +137,7 @@ WaterCube::WaterCube(Ogre::SceneManager& mSceneMgr, Ogre::Real x, Ogre::Real y, 
 WaterCube::~WaterCube()
 {
 }
-/*
-void WaterCube::accept(CubeVisitor& cV) {
-	cV.visit(*this);
-}*/
+
 
 WoodenCube::WoodenCube(Ogre::SceneManager& mSceneMgr, Ogre::Real x, Ogre::Real y, Ogre::Real z) : Cube(mSceneMgr, x, y, z)
 {
@@ -206,18 +149,7 @@ WoodenCube::WoodenCube(Ogre::SceneManager& mSceneMgr, Ogre::Real x, Ogre::Real y
 WoodenCube::~WoodenCube()
 {
 }
-/*
-void WoodenCube::accept(CubeVisitor& cV) {
-	cV.visit(*this);
-}*/
 
-/*
-void CubeVisitor::visit(WoodenCube& wC) {}
-void CubeVisitor::visit(MetalCube& mC) {}
-void CubeVisitor::visit(WaterCube& wC) {}
-void CubeVisitor::visit(FireCube& fC) {}
-void CubeVisitor::visit(GoldCube& gC) {}
-*/
 CubeFactory& CubeFactory::getInstance()
 {
 	static CubeFactory instance; 
